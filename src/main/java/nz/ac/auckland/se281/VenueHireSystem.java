929d1c38fbed96dc68286836fa335728c2933a7f
Glen <glenmalbog30@gmail.com>
2024-03-24 14:52:46 +1300

    created class to check if a string can be converted to a number

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -11,11 +11,23 @@ class Venue {
   String venue_code;
   String capacity_input;
   String hire_fee;
-  Venue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+  public Venue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     venue_name = venueName;
     venue_code = venueCode;
     capacity_input = capacityInput;
     hire_fee = hireFeeInput;
+  }
+}
+
+// checks if a string is an integer
+class canParseInt {
+  public boolean main(String string) {
+    try {
+      int num = Integer.parseInt(string);
+      return true;
+    } catch (NumberFormatException e) {
+      return false;
+    }
   }
 }
 
@@ -34,18 +46,25 @@ public class VenueHireSystem {
   public void createVenue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     // TODO implement this method
     Venue new_venue = new Venue(hireFeeInput, hireFeeInput, hireFeeInput, hireFeeInput);
-    for () {
-      
+    int venue_id = -1;
+    for (int i=0; i < this.venue_list.size();i++) {
+      if (venueCode == this.venue_list.get(i).venue_code) {
+        venue_id = i;
       }
-    if ()
-      System.err.println();
-    else if ()
-       
+    }
+    if (venueName.replaceAll("\\s", "").isEmpty())
+      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+    else if (venue_id < 0)
+      MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode,this.venue_list.get(venue_id).venue_code);
     else {
-      new_venue.add(venueName);
-      new_venue.add(venueCode);
-      new_venue.add(capacityInput);
-      new_venue.add(hireFeeInput);
+      try {
+        Integer.parseInt(hireFeeInput);
+        Integer.parseInt(capacityInput);  
+      } catch (NumberFormatException e) {
+        // TODO: handle exception
+
+      }
+
       this.venue_list.add(new_venue);
       MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName,venueCode);
     }
