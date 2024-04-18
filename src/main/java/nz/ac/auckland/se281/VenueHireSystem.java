a58b920c5f2478f43fa1c1370597d3b894bec6de
Glen <glenmalbog30@gmail.com>
2024-04-18 23:04:52 +1200

    made printLoop function for the printVenues function to reuse and made abstract class with venue parameters that both Venues class and Booking class inherit

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
-class Venue {
-  String venue_name;
-  String venue_code;
-  String capacity_input;
+abstract class venueParameters {
+  protected String venue_name;
+  protected String venue_code;
+  protected String capacity;
+}
+
+class Venue extends venueParameters {
   String hire_fee;
 
   public Venue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     venue_name = venueName;
     venue_code = venueCode;
-    capacity_input = capacityInput;
+    capacity = capacityInput;
     hire_fee = hireFeeInput;
   }
 }
 
-class Booking {
-  String venue_name;
-  String venue_code;
-  String adj_capacity;
+class Booking extends venueParameters {
+  String capacity;
   String date_booked;
   String booking_reference;
 
@@ -33,7 +34,7 @@ class Booking {
       String booking_reference) {
     this.venue_name = venueName;
     this.venue_code = venueCode;
-    this.adj_capacity = adjCapacity;
+    this.capacity = adjCapacity;
     this.date_booked = date_booked;
     this.booking_reference = booking_reference;
   }
@@ -91,89 +92,98 @@ public class VenueHireSystem {
         + year.toString();
   }
 
+  // loop used in printVenues to print all the venues
+  private void printLoop(int size) {
+    for (int i = 0; i < size; i++) {
+      MessageCli.VENUE_ENTRY.printMessage(
+          ((Venue) this.venue_list.get(i)).venue_name,
+          ((Venue) this.venue_list.get(i)).venue_code,
+          ((Venue) this.venue_list.get(i)).capacity,
+          ((Venue) this.venue_list.get(i)).hire_fee,
+          checkBookings(((Venue) this.venue_list.get(i)).venue_code));
+    }
+  }
+
   public void printVenues() {
     Integer size = this.venue_list.size();
+    // changes wording of the header (plural vs singular) depending on how many venues in the list
     if (size <= 0) {
       MessageCli.NO_VENUES.printMessage();
     } else if (size == 1) {
       MessageCli.NUMBER_VENUES.printMessage("is", numToWords.main(size), "");
-      for (int i = 0; i < size; i++) {
-        MessageCli.VENUE_ENTRY.printMessage(
-            ((Venue) this.venue_list.get(i)).venue_name,
-            ((Venue) this.venue_list.get(i)).venue_code,
-            ((Venue) this.venue_list.get(i)).capacity_input,
-            ((Venue) this.venue_list.get(i)).hire_fee,
-            checkBookings(((Venue) this.venue_list.get(i)).venue_code));
-      }
+      printLoop(size);
     } else if (size < 10) {
       MessageCli.NUMBER_VENUES.printMessage("are", numToWords.main(size), "s");
-      for (int i = 0; i < size; i++) {
-        MessageCli.VENUE_ENTRY.printMessage(
-            ((Venue) this.venue_list.get(i)).venue_name,
-            ((Venue) this.venue_list.get(i)).venue_code,
-            ((Venue) this.venue_list.get(i)).capacity_input,
-            ((Venue) this.venue_list.get(i)).hire_fee,
-            checkBookings(((Venue) this.venue_list.get(i)).venue_code));
-      }
+      printLoop(size);
     } else if (size >= 10) {
       MessageCli.NUMBER_VENUES.printMessage("are", size.toString(), "s");
-      for (int i = 0; i < size; i++) {
-        MessageCli.VENUE_ENTRY.printMessage(
-            ((Venue) this.venue_list.get(i)).venue_name,
-            ((Venue) this.venue_list.get(i)).venue_code,
-            ((Venue) this.venue_list.get(i)).capacity_input,
-            ((Venue) this.venue_list.get(i)).hire_fee,
-            checkBookings(((Venue) this.venue_list.get(i)).venue_code));
+      printLoop(size);
+    }
+  }
+
+  // checks if new venue has an empty venue name
+  private int emptyVenueName(String venueName, int fails) {
+    if (venueName.replaceAll("\\s", "").isEmpty()) {
+      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+      fails++;
+    }
+    return fails;
+  }
+
+  // checks if the capacity input for the new venue is valid (integer)
+  private int invalidCapacity(String capacityInput, int fails) {
+    if (!canParseInt.main(capacityInput)) {
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", "");
+      fails++;
+    } else if (Integer.parseInt(capacityInput) < 0) {
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
+      fails++;
+    }
+    return fails;
+  }
+
+  // checks if the hire fee input is a positive number for the new venue
+  private int positiveHireFee(String hireFeeInput, int fails) {
+    if (!canParseInt.main(hireFeeInput)) {
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
+      fails++;
+    } else if (Integer.parseInt(hireFeeInput) < 0) {
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
+      fails++;
+    }
+    return fails;
+  }
+
+  // checks the venue list if there is already a venue with the same venue code
+  private int duplicateCode(int fails, String venueCode, String venueName) {
+    String v_name = "";
+    for (int i = 0; i < this.venue_list.size(); i++) {
+      //  uses loop to check if there is a duplicate venue code
+      if (((Venue) this.venue_list.get(i)).venue_code.contains(venueCode)) {
+        v_name = ((Venue) this.venue_list.get(i)).venue_name;
+        MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, v_name);
+        fails++;
+        // ends loop if an identical venue code is found
+        break;
       }
     }
+    return fails;
   }
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     Venue new_venue = new Venue(venueName, venueCode, capacityInput, hireFeeInput);
-    Boolean valid_venue = true;
-    if (venueName.replaceAll("\\s", "").isEmpty()) {
-      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
-      valid_venue = false;
-    }
+    // checks all fail cases and counts how many fail cases there are
+    int fails = 0;
+    fails = emptyVenueName(venueName, fails);
+    fails = invalidCapacity(capacityInput, fails);
+    fails = positiveHireFee(hireFeeInput, fails);
+    fails = duplicateCode(fails, venueCode, venueName);
 
-    if (!canParseInt.main(capacityInput)) {
-      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", "");
-      valid_venue = false;
-    } else if (Integer.parseInt(capacityInput) < 0) {
-      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
-      valid_venue = false;
-    }
-
-    if (!canParseInt.main(hireFeeInput)) {
-      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
-      valid_venue = false;
-    } else if (Integer.parseInt(hireFeeInput) < 0) {
-      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
-      valid_venue = false;
-    }
-
-    String v_name = "";
-    if (valid_venue) {
-      if (this.venue_list.size() == 0) {
-        this.venue_list.add(new_venue);
-        MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
-      } else {
-        for (int i = 0; i < this.venue_list.size(); i++) {
-          //  uses loop to check if there is a duplicate venue code
-          if (((Venue) this.venue_list.get(i)).venue_code.contains(venueCode)) {
-            v_name = ((Venue) this.venue_list.get(i)).venue_name;
-            MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, v_name);
-            // ends loop if an identical venue code is found
-            break;
-          }
-          // creates the new entry if the loop is complete and there is no duplicate code
-          else if (i == this.venue_list.size() - 1) {
-            this.venue_list.add(new_venue);
-            MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
-          }
-        }
-      }
+    // creates new venue to venue list if there are no issues/fails
+    if (fails <= 0) {
+      this.venue_list.add(new_venue);
+      MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
     }
   }
 
@@ -278,7 +288,7 @@ public class VenueHireSystem {
       for (int i = 0; i < this.venue_list.size(); i++) {
         if ((((Venue) this.venue_list.get(i)).venue_code.contains(options[0]))) {
           ven_name = ((Venue) this.venue_list.get(i)).venue_name;
-          ven_capacity = Integer.parseInt(((Venue) this.venue_list.get(i)).capacity_input);
+          ven_capacity = Integer.parseInt(((Venue) this.venue_list.get(i)).capacity);
           break;
         }
       }
@@ -327,6 +337,7 @@ public class VenueHireSystem {
             this.booking_list.get(i).booking_reference, this.booking_list.get(i).date_booked);
       }
     }
+    // if there are no bookings
     if (!count) {
       MessageCli.PRINT_BOOKINGS_NONE.printMessage(name);
     }
